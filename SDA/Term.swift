468db308992046cf583a33f6f4e8199d0f67e8f6
MediBoss <mediassumani49@gmail.com>
2018-08-29 13:04:37 -0700

    Modeling JSON data

--- SDA/Term.swift
+++ SDA/Term.swift
@@ -13,9 +13,35 @@ struct Term: Decodable{
     let definition: String?
     let lexicalCategory: String?
     
-    
+
+    enum mainEntryKey: String, CodingKey{
+        case results
+    }
+    enum lexicalEntriesKey: String, CodingKey{
+        case lexicalEntries
+    }
+    enum entriesKey: String, CodingKey{
+        case entries
+    }
+    enum sensesKey: String, CodingKey{
+        case senses
+    }
+    enum definitions: String, CodingKey{
+        case definitions
+    }
+    enum mainKeys: String, CodingKey{
+        case definition
+        case lexicalCategory
+    }
     
     init(from decoder: Decoder) throws {
+        let mainEntryPoint = try decoder.container(keyedBy: mainEntryKey.self)
+        let lexicalEntryPoint = try mainEntryPoint.nestedContainer(keyedBy: lexicalEntryPoint.self, forKey: .results)
+        let entriesEntryPoint = try lexicalEntryPoint.nestedContainer(keydBy: entriesKey.self, forKey: .entries)
+        let sensesEntryPoint = try entriesEntryPoint.nestedConatiner(keyedBy: sensesKey.self, forKey: .senses)
+        let definitionEntryPoint = try sensesEntryPoint.nestedContainer(keyedBy: definitions.self. forKey: .definitions)
         
+        self.definition = try definitionEntryPoint.decode(String.self, forKey: .definition)
+        //self.lexicalCategory = try 
     }
 }
