6afe6934e4d15389b14258721ed87d0b16fa8822
MediBoss <mediassumani49@gmail.com>
2018-08-28 22:43:02 -0700

    working on networking

--- SDA/Networking/NetworkService.swift
+++ SDA/Networking/NetworkService.swift
@@ -9,15 +9,33 @@
 import Foundation
 
 struct NetworkService{
-    var word_id : String?
-    var completeURL: URL?
+    var word_id : String
+    var completeURL: URL
     
     init(word_id: String) {
         self.word_id = word_id
-        self.completeURL = URL(string: "\(Constants.baseURL)\(Constants.source_lang)")
+        self.completeURL = URL(string: "\(Constants.baseURL)"+"/"+"\(Constants.source_lang)" + "\(self.word_id)")!
+        
     }
     
     func makeAPIRequest(_ completionHandler: @escaping (Term) ->Void){
         
+        let request: URLRequest = URLRequest(url: self.completeURL)
+        let task = URLSession.shared.dataTask(with: request) { (dataFromApi, response, error) in
+            if error == nil{ // if there is no error during the api call
+                guard let httpResponse = response as? HTTPURLResponse else {return}
+                    // checking to see the response code from the api
+                switch httpResponse.statusCode{
+                case 200:
+                    
+                case 400:
+                    
+                default:
+                    
+                }
+                
+                
+            }
+        }
     }
 }
