0b2465cc0a27207b7b16fea57c0f6ee8c83e0378
MediBoss <mediassumani49@gmail.com>
2018-08-28 22:52:11 -0700

    done with Networking

--- SDA/Networking/NetworkService.swift
+++ SDA/Networking/NetworkService.swift
@@ -21,21 +21,35 @@ struct NetworkService{
     func makeAPIRequest(_ completionHandler: @escaping (Term) ->Void){
         
         let request: URLRequest = URLRequest(url: self.completeURL)
-        let task = URLSession.shared.dataTask(with: request) { (dataFromApi, response, error) in
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if error == nil{ // if there is no error during the api call
                 guard let httpResponse = response as? HTTPURLResponse else {return}
                     // checking to see the response code from the api
                 switch httpResponse.statusCode{
                 case 200:
-                    
-                case 400:
+                    guard let dataFromApi = data else {return}
+                    do{
+                            // Decoding the data from the API from JSON format to Term Object
+                        let decoder = JSONDecoder()
+                        let term = try decoder.decode(Term.self, from: dataFromApi)
+                        completionHandler(term)
+                        
+                    }catch let errorFromDataReceived{
+                            // if an error is found while decoding the JSON data
+                        print(errorFromDataReceived.localizedDescription)
+                    }
                     
                 default:
+                        // if a code other than 200 is returned
+                    print("HTTP CODE : \(httpResponse.statusCode)")
                     
                 }
                 
-                
+            }else{
+                    // if an error was found during the http request
+                guard let errorFound = error else {return}
+                print(errorFound.localizedDescription)
             }
-        }
+        }.resume()
     }
 }
