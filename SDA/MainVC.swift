cdbc1b22fbfad975dcc1aa10a8777969db327475
MediBoss <mediassumani49@gmail.com>
2018-08-30 16:40:14 -0700

    organizing files

--- SDA/MainVC.swift
+++ SDA/MainVC.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 class MainVC: UIViewController{
-    
+
     // - MARK : IBOULETS
     @IBOutlet weak var searchBar: UITextField!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.searchBar.delegate = self as UITextFieldDelegate
-     
+
     }
-    
+
     // - MARK : IBACTIONS
 
     @IBAction func searchButtonIsTaped(_ sender: Any) {
         guard let word = self.searchBar.text else {return}
         let networkRequest = NetworkService()
-        
+
         networkRequest.makeAPIRequest(word) { (Term) in
             guard let definition = Term.definition else {return}
             DispatchQueue.main.async {
