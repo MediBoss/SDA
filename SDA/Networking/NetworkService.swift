26896c451c7410f0948c97cf67ad37e61cb4232d
MediBoss <mediassumani49@gmail.com>
2018-08-30 16:09:17 -0700

    fxing api bug

--- SDA/Networking/NetworkService.swift
+++ SDA/Networking/NetworkService.swift
@@ -10,38 +10,37 @@ import Foundation
 
 struct NetworkService{
     
+    // MARK: Function to make the request to the Oxford API
     func makeAPIRequest(_ word: String,_ completionHandler: @escaping (Term) ->Void){
         
-            // Setting up the request to be made to the api
-        let completeURL = URL(string: "\(Constants.baseURL)/\(Constants.source_lang)/\(word)")!
-        var request: URLRequest = URLRequest(url: completeURL)
+        // MARK: Setting up the request to be made to the api
+        let url = URL(string: "\(Constants.baseURL)/\(Constants.source_lang)/\(word)")
+        var request: URLRequest = URLRequest(url: url!)
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("\(Constants.app_id)", forHTTPHeaderField: "app_id")
         request.addValue("\(Constants.apiKey)", forHTTPHeaderField: "app_key")
         
-            // Launching the GET request to the API
+        // MARK: Launching the GET request to the API
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if error == nil{ // if there is no error during the api call
                 guard let httpResponse = response as? HTTPURLResponse else {return}
-                    // checking to see the response code from the api
+                // MARK: evaluating teh JSON response from the API
                 switch httpResponse.statusCode{
                 case 200:
-                    guard let dataFromApi = data else {return}
                     do{
-                            // Decoding the data recievd from the server into a Term object
+                        guard let dataFromApi = data else {return}
+                        // MARK: Decoding the data recievd from the server into a Term object
                         let decoder = JSONDecoder()
                         let term = try decoder.decode(Term.self, from: dataFromApi)
                         completionHandler(term)
                     }catch let errorFromDataReceived{
-                            // if an error is found while decoding the JSON data
+                        //MARK: Checking for possible errors
                         print(errorFromDataReceived.localizedDescription)
                     }
                 default:
-                        // if a code other than 200 is returned
                     print("HTTP CODE : \(httpResponse.statusCode)")
                 }
             }else{
-                    // if an error was found during the http request
                 guard let errorFound = error else {return}
                 print(errorFound.localizedDescription)
             }
